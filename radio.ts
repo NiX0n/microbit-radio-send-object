eec4ecb072b6ae7733ff5f75ba6836b49aa0a261
NiX0n <BiggoronSword+GitHub@gmail.com>
2025-03-23 18:15:43 -0400

    Removed: Test-related code

--- radio.ts
+++ radio.ts
@@ -74,28 +74,3 @@ export function sendObject(obj: any)
 }
 
 } // namespace radio
-
-
-radio.onReceivedObject(function(receivedObject: any, props: any) {
-    // log() for debugging
-    console.log(receivedObject)
-})
-
-// Test
-input.onButtonPressed(Button.A, function () {
-    radio.sendObject({ str: "thisisastringlongerthanapacket", arr: [1, 2, 3], bool: true })
-})
-
-// Constant must be same for all devices
-let RADIO_GROUP: number = 11
-// Send serialNumber for addressing
-radio.setTransmitSerialNumber(true)
-radio.setGroup(RADIO_GROUP)
-
-// Handshake
-radio.sendString("null")
-
-
-basic.forever(function () {
-	
-})
